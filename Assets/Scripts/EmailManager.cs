20c1c0757c69e22d7af87d1369ea0830a52062f1
Jirashy <jirachai@madvrstdio.com>
2020-02-11 10:07:48 +0700

    git Init

---
+++ Assets/Scripts/EmailManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+public class EmailManager : MonoBehaviour
+{
+    [SerializeField]
+    private string path;
+    public void SendEmail() 
+    {
+        MailMessage mail = new MailMessage();
+
+        mail.From = new MailAddress("jirachai@madvrstudio.com");
+        mail.To.Add("divinetrees@gmail.com");
+        mail.Subject = "Test";
+        mail.Body = "Your pictures";
+        mail.Attachments.Add(new Attachment(path));
+
+        SmtpClient smtpServer = new SmtpClient("smtp.gmail.com");
+        smtpServer.Port = 587;
+        smtpServer.Credentials = new System.Net.NetworkCredential("jirachai@madvrstudio.com", "") as ICredentialsByHost;
+        smtpServer.EnableSsl = true;
+        ServicePointManager.ServerCertificateValidationCallback = delegate(object s, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        {return true;};
+        smtpServer.Send(mail);
+    }
+}
